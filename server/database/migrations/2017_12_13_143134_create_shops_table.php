803fda8d14704fc91f8623e23e7a4914075c4aeb
ouabou abdelhakim <hakim.ouabou@gmail.com>
2017-12-16 18:14:31 +0000

    shops migration
    Create shops table

--- server/database/migrations/2017_12_13_143134_create_shops_table.php
+++ server/database/migrations/2017_12_13_143134_create_shops_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateShopsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name', 45);
+            $table->string('picture');
+            $table->string('email', 100);
+            $table->string('city', 45);
+            $table->string('location_type', 45);
+            $table->string('location_coordinates', 200);
         });
     }
 
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shops');
     }
 }
