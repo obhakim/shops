4e9d483284c48c96702a39268eaf4ab62d394520
ouabou abdelhakim <hakim.ouabou@gmail.com>
2017-12-17 13:38:45 +0000

    Add nav items to the default vue  app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container pt-2">
+      <div class="row">
+        <div class="col-12 text-right">
+          <ul class="nav justify-content-right float-right">
+            <li class="nav-item">
+              <router-link tag="a" :to="{name: 'Home'}" class="nav-link active">
+              Nearby Shops
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link tag="a" :to="{name: 'FavoriteShops'}" class="nav-link">
+              My prefered Shops
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container p-3">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    cursor: pointer;
+  }
 </style>
